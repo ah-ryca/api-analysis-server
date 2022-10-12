750a11ef6c4e85b90ce878fbe3af0fc35dfd1447
Saeed <hassanisaeed19@yahoo.com>
2022-10-12 18:30:28 +0330

    refactor type of dates

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class RequestInfo(models.Model):
     remote_addr = models.CharField(max_length=20, null=True)
     remote_user = models.CharField(max_length=50, null=True)
-    time_local = models.CharField(max_length=80, null=True)
+    time_local = models.DateTimeField(null=True)
     request = models.CharField(max_length=50, null=True)
     status = models.IntegerField( null=True)
     body_bytes_sent = models.IntegerField(null=True)
@@ -11,7 +11,7 @@ class RequestInfo(models.Model):
     http_user_agent = models.CharField(max_length=400, null=True)
     status_type = models.CharField(max_length=50, null=True)
     bytes_sent = models.IntegerField(null=True)
-    request_time = models.FloatField(null=True)
+    request_time =  models.DateTimeField(null=True)
     request_path = models.CharField(max_length=100, null=True)
     
     class Meta:
