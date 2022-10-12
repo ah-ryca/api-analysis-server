ce3177aef353531f3ce22de66fd3e494cac8c4b2
Saeed <hassanisaeed19@yahoo.com>
2022-10-12 18:30:11 +0330

    add request info model

---
+++ models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class RequestInfo(models.Model):
+    remote_addr = models.CharField(max_length=20, null=True)
+    remote_user = models.CharField(max_length=50, null=True)
+    time_local = models.CharField(max_length=80, null=True)
+    request = models.CharField(max_length=50, null=True)
+    status = models.IntegerField( null=True)
+    body_bytes_sent = models.IntegerField(null=True)
+    http_referer = models.CharField(max_length=100, null=True)
+    http_user_agent = models.CharField(max_length=400, null=True)
+    status_type = models.CharField(max_length=50, null=True)
+    bytes_sent = models.IntegerField(null=True)
+    request_time = models.FloatField(null=True)
+    request_path = models.CharField(max_length=100, null=True)
+    
+    class Meta:
+        db_table = 'request_info'
+ 
