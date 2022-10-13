1d11f87e626932c2c2bf0305ded6723eb7c143a2
Saeed <hassanisaeed19@yahoo.com>
2022-10-13 10:24:29 +0330

    refactor: make modular

--- core/models/request_info.py
+++ core/models/request_info.py
@@ -1,19 +1,21 @@
 from django.db import models
 
+__all__ = ('RequestInfo',)
+
+
 class RequestInfo(models.Model):
     remote_addr = models.CharField(max_length=20, null=True)
     remote_user = models.CharField(max_length=50, null=True)
     time_local = models.DateTimeField(null=True)
     request = models.CharField(max_length=50, null=True)
-    status = models.IntegerField( null=True)
+    status = models.IntegerField(null=True)
     body_bytes_sent = models.IntegerField(null=True)
     http_referer = models.CharField(max_length=100, null=True)
     http_user_agent = models.CharField(max_length=400, null=True)
     status_type = models.CharField(max_length=50, null=True)
     bytes_sent = models.IntegerField(null=True)
-    request_time =  models.DateTimeField(null=True)
+    request_time = models.DateTimeField(null=True)
     request_path = models.CharField(max_length=100, null=True)
-    
+
     class Meta:
         db_table = 'request_info'
- 
--- core/serializers/request_info.py
+++ core/serializers/request_info.py
@@ -1,12 +1,9 @@
- 
-
 from rest_framework.serializers import ModelSerializer
 
 from core.models import RequestInfo
 
 
 class RequestInfoSerializer(ModelSerializer):
-    
     class Meta:
         model = RequestInfo
         fields = '__all__'
