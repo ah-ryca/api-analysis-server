7ec92e2961bfae38fc772dbb1decef3e4ca20232
Saeed <hassanisaeed19@yahoo.com>
2022-10-14 22:58:22 +0330

    feat: add type of devices

---
+++ core/models/helper.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.utils.translation import gettext_lazy as _
+
+
+class TYPE(models.IntegerChoices):
+    IS_MOBILE = 1, _('موبایل')
+    IS_TABLET = 2, _('تبلت')
+    IS_PC = 3, _('کامپیوتر')
+    IS_TOUCH_CAPABLE = 4, _('لمسی')
+    IS_BOT = 5, _('بات')
+    OTHER = 6, _('نامشخص')
