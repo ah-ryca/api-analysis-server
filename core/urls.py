bc5a97c9f0ab8a1342d6e0f7d33c04b726a3090f
Saeed <hassanisaeed19@yahoo.com>
2022-10-14 20:59:29 +0330

    feat: show detail of a page

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,10 @@ from core.views.app_pages import AppPageViewSet
 
 urlpatterns = [
     path('v1/requests', RequestInfoViewSet.as_view({'get': 'list'}), name='requests'),
+
     path('v1/app/pages', AppPageViewSet.as_view({'get': 'list'}), name='list_of_pages'),
+    path('v1/app/page/<int:page_id>', AppPageViewSet.as_view({'get': 'get_page_detail'}), name='page_detail'),
+
+
     path('v1/api-stat/<str:path>', APIStatViewSet.as_view({'get': 'list'}), name='list_of_pages'),
 ]
--- core/views/app_pages.py
+++ core/views/app_pages.py
@@ -1,9 +1,68 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Page
+from core.serializers.api_stat import APIResult, APIStatSerializer
 from core.serializers.app_pages import PageListSerializer
 
 
 class AppPageViewSet(ModelViewSet):
     queryset = Page.objects.all().order_by('-id')
     serializer_class = PageListSerializer
+
+    def get_page_detail(self, request, *args, **kwargs):
+        page_id = kwargs.get('page_id')
+
+        apis = [f"TRIM(request_path) LIKE  '{path}%'" for path in
+                Page.objects.get(pk=page_id).apis.values_list('path', flat=True)]
+
+        filters = self.request.GET
+        date_from = filters.get('from')
+        date_to = filters.get('to')
+
+        api_conditions = ' OR '.join(apis)
+
+        print("........................")
+        print(api_conditions)
+
+        sql = f"""
+                SELECT
+                    1 as id,
+                    created_at,
+                    SUM(success) AS success,
+                    SUM(fail) AS fail
+                FROM
+                    (
+                        SELECT
+                            created_at,
+                            COUNT(success) AS success,
+                            COUNT(fail) AS fail
+                        FROM
+                            (
+                                SELECT
+                                    DATE(request_time) as created_at,
+                                    CASE
+                                        WHEN status >= 200
+                                        AND status < 300 THEN 200
+                                    END AS success,
+                                    CASE
+                                        WHEN status < 200
+                                        OR status >= 300 THEN 500
+                                    END AS fail
+                                FROM
+                                    request_info
+                                WHERE {api_conditions} AND request_time BETWEEN '{date_from}' AND '{date_to}'
+                            ) AS t1
+                        GROUP BY
+                            created_at,
+                            success,
+                            fail
+                    ) AS t2
+                GROUP BY
+                    created_at
+                ORDER BY
+                    created_at ASC;
+        """
+        queryset = APIResult.objects.raw(sql)
+        serializer = APIStatSerializer(queryset, many=True)
+        return Response(serializer.data)
