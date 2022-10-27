939a78e7daa7d13e3d258f6f813180046b5c8030
Saeed <hassanisaeed19@yahoo.com>
2022-10-27 10:40:20 +0330

    chore: formatting code

--- nginx.py
+++ nginx.py
@@ -1,13 +1,14 @@
-from __future__ import print_function
 import os
 import sys
 
-sys.path.append( os.path.join(os.path.dirname(__file__), 'app'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 import django
+
 django.setup()
 from core.models import RequestInfo
+
 
 def main():
     import gzip
@@ -20,7 +21,9 @@ def main():
 
     INPUT_DIR = "/var/log/nginx/"
 
-    lineformat = re.compile(r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""", re.IGNORECASE)
+    lineformat = re.compile(
+        r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""",
+        re.IGNORECASE)
 
     for f in os.listdir(INPUT_DIR):
         if f.endswith(".gz"):
@@ -36,7 +39,8 @@ def main():
             if data:
                 datadict = data.groupdict()
                 ip = datadict["ipaddress"]
-                datetimeobj = datetime.strptime(datadict["dateandtime"], "%d/%b/%Y:%H:%M:%S %z") # Converting string to datetime obj
+                datetimeobj = datetime.strptime(datadict["dateandtime"],
+                                                "%d/%b/%Y:%H:%M:%S %z")  # Converting string to datetime obj
                 url = datadict["url"]
                 bytessent = datadict["bytessent"]
                 referrer = datadict["refferer"]
@@ -45,20 +49,17 @@ def main():
                 method = data.group(6)
 
                 RequestInfo.objects.create(
-                                            remote_addr = ip,  
-                                            request_time =  tz.normalize(datetimeobj),
-                                            request_path =url,
-                                            status = status,
-                                            status_type = method,
-                                            bytes_sent = bytessent,
-                                            http_referer = referrer,
-                                            http_user_agent = useragent  
-                                             )
+                    remote_addr=ip,
+                    request_time=tz.normalize(datetimeobj),
+                    request_path=url,
+                    status=status,
+                    status_type=method,
+                    bytes_sent=bytessent,
+                    http_referer=referrer,
+                    http_user_agent=useragent
+                )
         logfile.close()
 
 
 if __name__ == '__main__':
     main()
-
-
-
